15d36a020e05a64ee00bb248c5a6c1f7732d7008
Emre Can Satık <emrecanstk@gmail.com>
2022-05-26 01:02:24 +0300

    added @PostMapping

--- src/main/java/com/emrecanstk/department/controller/DepartmentController.java
+++ src/main/java/com/emrecanstk/department/controller/DepartmentController.java
@@ -3,18 +3,24 @@ package com.emrecanstk.department.controller;
 
 import com.emrecanstk.department.entity.Department;
 import com.emrecanstk.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
 
-    public Department saveDepartment(Department department) {
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+        log.info("Inside saveDepartment method of DepartmentController.");
         return departmentService.saveDepartment(department);
     }
 }
