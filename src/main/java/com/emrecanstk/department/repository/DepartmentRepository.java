5c57c057a7848f0e71caa1e7fa6653fc957fa879
Emre Can Satık <emrecanstk@gmail.com>
2022-05-26 00:54:31 +0300

    added annotations

---
+++ src/main/java/com/emrecanstk/department/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.emrecanstk.department.repository;
+
+
+import com.emrecanstk.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
